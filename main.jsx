1bfc79c0ce514653c88afdbbcfa0a8e1aa259fc2
Blaze92Codi <firefighter2501@gmail.com>
2026-05-04 21:27:06 -0400

    Refactor imports and component rendering in main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import StayDryPlatform from './App.jsx';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./style.css";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StayDryPlatform />
+    <App />
   </React.StrictMode>
 );
